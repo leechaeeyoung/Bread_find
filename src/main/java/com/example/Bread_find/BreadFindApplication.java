44997c0c98787f4a959584876858286a611b3e62
leechaeeyoung <zs1397@naver.com>
2023-09-13 20:56:51 +0900

    change login system

--- src/main/java/com/example/Bread_find/BreadFindApplication.java
+++ src/main/java/com/example/Bread_find/BreadFindApplication.java
@@ -2,8 +2,11 @@ package com.example.Bread_find;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.example.Bread_find.repository")
 public class BreadFindApplication {
 
 	public static void main(String[] args) {
