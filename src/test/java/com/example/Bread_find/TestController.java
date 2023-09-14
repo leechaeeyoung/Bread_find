9a6683e1c4c2c45cb30eda7a5830962723d75a47
leechaeeyoung <zs1397@naver.com>
2023-09-14 20:18:17 +0900

    kakao login setting

---
+++ src/test/java/com/example/Bread_find/TestController.java
@@ -0,0 +1,13 @@
+package com.example.Bread_find;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String test() {
+        return "Hello! This is Domain Test!";
+    }
+}
