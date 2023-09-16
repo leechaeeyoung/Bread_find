67d8b36dbf5d3f5192f7b2208ba048ffcc0b9564
leechaeeyoung <zs1397@naver.com>
2023-09-16 16:57:31 +0900

    카카오 로그인 구현 1

--- src/main/java/com/example/Bread_find/controller/KakaoController.java
+++ src/main/java/com/example/Bread_find/controller/KakaoController.java
@@ -1,39 +1,12 @@
 package com.example.Bread_find.controller;
 
-import com.example.Bread_find.service.KakaoService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import java.io.IOException;
-import java.util.Map;
 
 @Controller
-@RequestMapping("/member")
 public class KakaoController {
-    @Autowired
-    KakaoService ks;
-
-    @GetMapping("/do")
-    public String loginPage()
-    {
-        return "kakaoLogin.html";
+    @GetMapping("/login")
+    public String loginPage(){
+        return "login";
     }
-
-    @GetMapping("/kakao")
-    public String getCI(@RequestParam String code, Model model) throws IOException {
-        System.out.println("code = " + code);
-        String access_token = ks.getToken(code);
-        Map<String, Object> userInfo = ks.getUserInfo(access_token);
-        model.addAttribute("code", code);
-        model.addAttribute("access_token", access_token);
-        model.addAttribute("userInfo", userInfo);
-
-        //ci는 비즈니스 전환후 검수신청 -> 허락받아야 수집 가능
-        return "index.html";
-    }
-
 }
