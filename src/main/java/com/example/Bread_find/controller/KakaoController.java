53819a2a568006066daf82ad474375fe6a40541d
leechaeeyoung <zs1397@naver.com>
2023-09-17 15:47:56 +0900

    api변경

--- src/main/java/com/example/Bread_find/controller/KakaoController.java
+++ src/main/java/com/example/Bread_find/controller/KakaoController.java
@@ -5,6 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class KakaoController {
+    @GetMapping("/main")
+    public String mainPage(){
+        return "main";
+    }
     @GetMapping("/login")
     public String loginPage(){
         return "login";
