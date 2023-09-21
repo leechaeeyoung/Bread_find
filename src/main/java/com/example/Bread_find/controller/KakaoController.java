305c34af88034220d7c094c7b46eebeccb362a61
leechaeeyoung <zs1397@naver.com>
2023-09-21 18:13:34 +0900

    kakaoLogin 구현

--- src/main/java/com/example/Bread_find/controller/KakaoController.java
+++ src/main/java/com/example/Bread_find/controller/KakaoController.java
@@ -1,20 +1,12 @@
 package com.example.Bread_find.controller;
 
-import com.example.Bread_find.service.KakaoService;
-import org.json.simple.parser.ParseException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.io.IOException;
-import java.util.Map;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class KakaoController {
-    @Autowired
-    KakaoService ks;
 
     @GetMapping("/main")
     public String mainPage(){
@@ -25,16 +17,8 @@ public class KakaoController {
         return "login";
     }
 
-    @GetMapping("/kakao")
-    public String getCI(@RequestParam String code, Model model) throws IOException, ParseException {
-        System.out.println("code = "+code);
-        String access_token = ks.getToken(code);
-        Map<String, Object> userInfo = ks.getUserInfo(access_token);
-        model.addAttribute("code",code);
-        model.addAttribute("access_token", access_token);
-        model.addAttribute("userInfo",userInfo);
-
-        // ci 비지니스 전환후 검수신청
-        return "index";
+    @GetMapping("/member/kakao")
+    public @ResponseBody String kakaoCallback(@RequestParam String code){
+        return "카카오 인증 완료"+code;
     }
 }
